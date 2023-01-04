aafd121d624b044c13a22895a9823a6ed2a2acf2
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-04 19:08:22 +0530

    MainProjectInfo Content added

--- src/Components/Projects/MainProject.js
+++ src/Components/Projects/MainProject.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import SectionTitle from '../UI/SectionTitle';
 import classes from "./MainProject.module.css";
+import MainProjectInfo from './MainProjectInfo';
 
 const MainProject = () => {
     return(
         <>
         <SectionTitle sectionNumber="02" title="Somethings I've built"></SectionTitle>
-
+        <MainProjectInfo />
         </>
     )
 }
--- src/Components/Projects/MainProjectInfo.js
+++ src/Components/Projects/MainProjectInfo.js
@@ -1,10 +1,39 @@
 import React from "react";
 import classes from './MainProjectInfo.module.css';
+import MyYuMeals from "../../Assets/MyYuMeals.jpg";
 
 const MainProjectInfo = () => {
+
     return (
-        <>
-        </>
+        <div className={classes.mainProjectInfo}>
+            <div className={classes.image}>
+                <img src={MyYuMeals} alt="Project image" />
+            </div>
+            <div className={classes.content}>
+                <p>Featured Project</p>
+                <h3>MyYuMeals</h3>
+                <div className={classes.projectDescription}>
+                    <p>
+                        Here I've tried to develop a food order app which gets inspired from online 
+                        food delivery services like Swiggy and Zomato in India. I have tried to use many
+                        react concepts like hooks and api context to complete the app. Currently I am 
+                        trying to add a bit of backend to it !
+                    </p>
+                </div>
+                <ul>
+                    <li>React</li>
+                    <li>React-Hooks</li>
+                    <li>Context-API</li>
+                    <li>Reducer</li>
+                </ul>
+                <div className={classes.projectLinks}>
+                    <ul>
+                        <li>Github</li>
+                        <li>Website</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
 
