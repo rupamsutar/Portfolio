1ee89aba61d8558631d4d1f555c3c8638761f12f
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-04 20:07:16 +0530

    CSS Fixed and content intro updated

--- src/Components/Projects/MainProjectInfo.js
+++ src/Components/Projects/MainProjectInfo.js
@@ -5,33 +5,38 @@ import MyYuMeals from "../../Assets/MyYuMeals.jpg";
 const MainProjectInfo = () => {
 
     return (
+
         <div className={classes.mainProjectInfo}>
             <div className={classes.image}>
                 <img src={MyYuMeals} alt="Project image" />
             </div>
-            <div className={classes.content}>
+            <div className={classes['content__intro']}>
                 <p>Featured Project</p>
+            </div>
+            <div className={classes['content__heading']}>
                 <h3>MyYuMeals</h3>
-                <div className={classes.projectDescription}>
-                    <p>
-                        Here I've tried to develop a food order app which gets inspired from online 
-                        food delivery services like Swiggy and Zomato in India. I have tried to use many
-                        react concepts like hooks and api context to complete the app. Currently I am 
-                        trying to add a bit of backend to it !
-                    </p>
-                </div>
+            </div>
+            <div className={classes['content__description']}>
+                <p>
+                    Here I've tried to develop a food order app which gets inspired from online 
+                    food delivery services like Swiggy and Zomato in India. I have tried to use many
+                    react concepts like hooks and api context to complete the app. Currently I am 
+                    trying to add a bit of backend to it !
+                </p>
+            </div>
+            <div className={classes['content__list']}>
                 <ul>
                     <li>React</li>
                     <li>React-Hooks</li>
                     <li>Context-API</li>
                     <li>Reducer</li>
                 </ul>
-                <div className={classes.projectLinks}>
-                    <ul>
-                        <li>Github</li>
-                        <li>Website</li>
-                    </ul>
-                </div>
+            </div>
+            <div className={classes['content__links']}>
+                <ul>
+                    <li>Github</li>
+                    <li>Website</li>
+                </ul>
             </div>
         </div>
     )
