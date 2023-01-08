73dc0fb38719b2a99e1c8ec2e205d6f8663379c5
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-08 09:47:46 +0530

    Icons updated from brittany chiang

--- src/Components/Projects/MainProjectInfo.js
+++ src/Components/Projects/MainProjectInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from './MainProjectInfo.module.css';
 import MyYuMeals from "../../Assets/MyYuMeals.jpg";
-import GithubIcon from "../../Assets/Icons/Github.png"
+// import GithubIcon from "../../Assets/Icons/Github.png"
 
 const MainProjectInfo = () => {
 
@@ -9,7 +9,7 @@ const MainProjectInfo = () => {
 
         <div className={classes.mainProjectInfo}>
             <div className={classes.image}>
-                <img src={MyYuMeals} alt="Project image" />
+                <img src={MyYuMeals} alt="Project information" />
             </div>
             <div className={classes['content__intro']}>
                 <p>Featured Project</p>
@@ -36,7 +36,20 @@ const MainProjectInfo = () => {
             <div className={classes['content__links']}>
                 <ul>
                     <li>
-                        <svg 
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        height="24"
+                        width="24"
+                        viewBox="0 0 24 24" 
+                         
+                        stroke="#e6f1ff" 
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                        >                            
+                            <path 
+                                d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                            </path>
+                    </svg>
+                        {/* <svg 
                             xmlns="http://www.w3.org/2000/svg" 
                             width="28" 
                             height="28" x="0" y="0" 
@@ -50,10 +63,25 @@ const MainProjectInfo = () => {
                                     fill="#e6f1ff"  >
                                 </path>
                             </g>
-                        </svg>
+                        </svg> */}
                     </li>
                     <li>
                         <svg 
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="26"
+                            width="26"
+                            viewBox="0 0 24 24"
+                            stroke="#e6f1ff" 
+                            stroke-width="2" 
+                            stroke-linecap="round" 
+                            stroke-linejoin="round">
+                                <path 
+                                    d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6">
+                                </path>
+                                <polyline points="15 3 21 3 21 9"></polyline>
+                                <line x1="10" y1="14" x2="21" y2="3"></line>
+                        </svg>
+                        {/* <svg 
                             xmlns="http://www.w3.org/2000/svg" 
                             width="28" height="28" x="0" y="0" 
                             viewBox="0 0 16.933333 16.933334">
@@ -66,7 +94,7 @@ const MainProjectInfo = () => {
                             </path>
                             </g>
                             </g>
-                        </svg>
+                        </svg> */}
                     </li>
                 </ul>
             </div>
