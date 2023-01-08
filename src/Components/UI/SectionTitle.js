4c6f12b4365f75f0b2d4ae6acb67cdeafc70522d
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-08 19:56:24 +0530

    section margin bottom corrrected

--- src/Components/UI/SectionTitle.js
+++ src/Components/UI/SectionTitle.js
@@ -3,9 +3,9 @@ import classes from "./SectionTitle.module.css"
 
 const SectionTitle = (props) => {
     return (
-        <>
+        <div className={classes.sectionTitle}>
             <h1 className={classes.h1}><span>{props.sectionNumber}.</span> {props.title}</h1>
-        </>
+        </div>
     )
 }
 
