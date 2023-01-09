028db5a3b6505e04af9973ddc9747fbfd7319448
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-09 13:24:13 +0530

    props passed into projects

--- src/Components/Projects/OtherProjects.js
+++ src/Components/Projects/OtherProjects.js
@@ -2,8 +2,9 @@ import React from "react";
 import classes from "./OtherProjects.module.css"
 import ProjectTemplate from "./ProjectTemplate"
 
-const Projects = [
+const projects = [
     {
+        projectNo: 1,
         githubLink: "https://rupamsutar.github.io/Design-Fundamentals/",
         projectLink: "https://github.com/rupamsutar/Design-Fundamentals",
         title: "Design Fundamentals",
@@ -16,7 +17,11 @@ const OtherProjects = () => {
     return (
         <div className={classes.otherProjects}>
             <h2>Other Noteworthy Projects</h2>
-            <ProjectTemplate />
+            <div className={classes['project-grid']}>
+                {projects.map((project) => (
+                    <ProjectTemplate {...project} />
+                ))}        
+            </div>    
         </div>
     )
 }
--- src/Components/Projects/ProjectTemplate.js
+++ src/Components/Projects/ProjectTemplate.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./ProjectTemplate.module.css"
 
-const ProjectTempplate = () => {
+const ProjectTemplate = (props) => {
     return (
         <div className={classes.mainBox}>
             <div className={classes["mainBox__one"]}>
@@ -15,6 +15,7 @@ const ProjectTempplate = () => {
                     </svg>
                 </div>
                 <div className={classes['svg-links']}>
+                    <a  href={props.githubLink} target="_blank" rel="noreferrer">
                     <svg 
                         xmlns="http://www.w3.org/2000/svg" 
                         viewBox="0 0 24 24"
@@ -24,6 +25,8 @@ const ProjectTempplate = () => {
                                 d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
                             </path>
                     </svg>
+                    </a>
+                    <a href={props.projectLink} target="_blank" rel="noreferrer">
                     <svg 
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24"
@@ -32,24 +35,25 @@ const ProjectTempplate = () => {
                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                             <polyline points="15 3 21 3 21 9"></polyline>
                             <line x1="10" y1="14" x2="21" y2="3"></line>
-                        </svg>
+                    </svg>
+                    </a>
                 </div>
             </div>
             <div className={classes["mainBox__two"]}>
-                <h3>Design Fundamentals</h3>
+                <h3>{props.title}</h3>
             </div>
             <div className={classes["mainBox__three"]}>
                 <p>
-                    Building a beautiful website to learn the design fundamentals and apply all the design knowledge
+                    {props.description}
                 </p>
             </div>
             <ul className={classes["mainBox__four"]}>
-                <li>Design</li>
-                <li>HTML</li>
-                <li>CSS</li>
+                {props.skills.map((skill) => (
+                    <li key={Math.random()}>{skill}</li>
+                ))}
             </ul>
         </div>
     )
 }
 
-export default ProjectTempplate;
+export default ProjectTemplate;
