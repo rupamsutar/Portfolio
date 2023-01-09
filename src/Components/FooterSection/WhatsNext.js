d169f54a3ced8aabc7b67c8b359ef7c83a08b6d8
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-09 21:32:43 +0530

    Whats Next setion completed

--- src/Components/FooterSection/WhatsNext.js
+++ src/Components/FooterSection/WhatsNext.js
@@ -5,7 +5,7 @@ const WhatsNext = () => {
     return (
         <div className={classes['main-section']}>
             <div className={classes['main-section-intro']}>
-                <p>04 What's Next ?</p>
+                <p>What's Next ?</p>
             </div>
             <div className={classes['main-section-heading']}>
                 <h1>Get In Touch</h1>
@@ -13,13 +13,13 @@ const WhatsNext = () => {
             <div className={classes['main-section-objective']}>
                 <p>
                     I am seeking challenging opportunities where I can utilize my skills for the success of the organization. 
-                    My Inbox is always open, if wanna get in , feel free to say hii ! 
+                    My Inbox is always open, if wanna get in touch, feel free to say hii ! 
                     I ll get back to you ASAP.
                 </p>
             </div>
-            <div>
-                <a href="mainto:rupamsutar281998@gmail.com">
-                    <button>Say Hello</button>
+            <div className={classes['main-section-button']}>
+                <a href="mailto:rupamsutar281998@gmail.com">
+                    Say Hello !
                 </a>
             </div>
         </div>
