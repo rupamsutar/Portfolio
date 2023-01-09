5958ebde7929657d04695d67114eb20984a7815d
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-09 10:44:11 +0530

    Project array added in OtherProjects module

--- src/Components/Projects/OtherProjects.js
+++ src/Components/Projects/OtherProjects.js
@@ -2,6 +2,16 @@ import React from "react";
 import classes from "./OtherProjects.module.css"
 import ProjectTemplate from "./ProjectTemplate"
 
+const Projects = [
+    {
+        githubLink: "https://rupamsutar.github.io/Design-Fundamentals/",
+        projectLink: "https://github.com/rupamsutar/Design-Fundamentals",
+        title: "Design Fundamentals",
+        description: "Building a beautiful website to learn the design fundamentals and apply all the design knowledge.",
+        skills: ["Design", "HTML", "CSS"]
+    }
+]
+
 const OtherProjects = () => {
     return (
         <div className={classes.otherProjects}>
