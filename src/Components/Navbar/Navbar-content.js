260cffcb65c79c6b58fe0df8b9c3f9f3a040c09f
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-10 11:09:13 +0530

    Navbar update

--- src/Components/Navbar/Navbar-content.js
+++ src/Components/Navbar/Navbar-content.js
@@ -6,9 +6,9 @@ const NavbarContent = () => {
         <>
             <ul className={classes.navContent}>
                 <li><span className={classes.navNum}>01.</span> <a href="#aboutMe">About</a></li>
-                <li><span className={classes.navNum}>02.</span> <a href="#contact">Contact</a></li>
                 <li><span className={classes.navNum}>03.</span> <a href="#work">Work</a></li>
-                <li><span className={classes.navNum}>04.</span> <a href={resume} target="_blank" rel="noreferrer">Resume</a></li>
+                <li><span className={classes.navNum}>02.</span> <a href="#contact">Contact</a></li>
+                <li><span className={classes.navNum}>04.</span> <a href={resume} target="_blank" rel="noreferrer" className={classes['resume-button']}>Resume</a></li>
             </ul>
         </>
     )
