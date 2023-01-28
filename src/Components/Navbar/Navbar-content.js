426b25a8dc2ceacd8677d25c086a7ad8312d9b48
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-28 18:26:23 +0530

    navbar icon added

--- src/Components/Navbar/Navbar-content.js
+++ src/Components/Navbar/Navbar-content.js
@@ -4,6 +4,16 @@ import resume from "../../Assets/resume.pdf"
 const NavbarContent = () => {
     return(
         <>
+            <input type="checkbox" id="check" className={classes['input']}></input>
+            <label htmlFor="check">
+                <svg 
+                    className={classes["checkbox-icon"]}
+                    xmlns="http://www.w3.org/2000/svg" 
+                    viewBox="0 0 24 24">
+                    <path  
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </label>
             <ul className={classes.navContent}>
                 <li><span className={classes.navNum}>01.</span> <a href="#aboutMe">About</a></li>
                 <li><span className={classes.navNum}>03.</span> <a href="#work">Work</a></li>
